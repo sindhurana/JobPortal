c2eb6854c2149815a71c742a91586dee08137ec0
sindhurana <rana.sindhu15@gmail.com>
2024-08-04 18:52:21 +0530

    started frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { Footer } from './components/Footer'
+import Home from './pages/Home'
+import Jobs from './pages/Jobs'
+import Dashboard from './pages/Dashboard'
+import PostApplication from './pages/PostApplication'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import "bootstrap/dist/bootstrap.min.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/jobs' element={<Jobs />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/post/application/:jobId' element={<PostApplication />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import TopNiches from '../components/TopNiches'
+import HowItWorks from '../components/HowItWorks'
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <TopNiches />
+            <HowItWorks />
+        </>
+    )
+}
